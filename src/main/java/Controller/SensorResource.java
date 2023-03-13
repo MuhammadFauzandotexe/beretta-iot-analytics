2d8ef5240d75961371af4b487f46d4cbd7130938
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2023-03-13 17:10:18 +0700

    menambahkan file baru

---
+++ src/main/java/Controller/SensorResource.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import Model.Channel;
+import Model.Sensor;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.List;
+
+@Path("updateSensor")
+public class SensorResource {
+    @GET
+    public List<Sensor> getData(){
+        return Sensor.listAll();
+    }
+    @POST
+    @Transactional
+    @Path("{apikey}")
+    public Sensor addSensor(@PathParam("apikey")String apikey, Sensor sensor){
+        Channel channel = Channel.findById(apikey);
+        channel.sensorList.add(sensor);
+        sensor.persist();
+        return sensor;
+    }
+}
